8c2ea223b40427440078c6281606d48a6672f11c
Jason Ren <renjiawei1997@gmail.com>
2018-04-16 11:37:57 +0800

    virtual env

---
+++ env/sgintern_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jason/env/sgintern_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ go.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+pwd
+source ./env/sgintern_env/bin/activate 
+echo "Enter targeted folder."
