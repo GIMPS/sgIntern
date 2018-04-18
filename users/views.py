5925ac3bc03ce18020ccb3dd3789438f5760d6ba
dabaitudiu <bigbroase@163.com>
2018-04-18 16:58:58 +0800

    fixed.

--- users/views.py
+++ users/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render,redirect
 from .forms import RegisterForm
+from django.contrib.auth import login as auth_login
 
 def register(request):
+    redirect_to = request.POST.get('next',request.GET.get('next',''))
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            user = form.save()
+            auth_login(request, user)
+            if redirect_to:
+                return redirect(redirect_to)
+            else:
+                return redirect('/')
     else:
         form = RegisterForm()
-    return render(request,'users/register.html',context={'form':form})
+    return render(request,'users/register.html',context={'form':form, 'next':redirect_to})
